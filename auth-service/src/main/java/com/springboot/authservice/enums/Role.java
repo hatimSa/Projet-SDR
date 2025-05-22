f0408cf0342474ff99a8616531d5a11d4a9122bd
abdelhamidsoussi <122819227+abdelhamidsoussi@users.noreply.github.com>
2025-05-22 12:57:15 +0100

    Ajout du service d'authentification (auth-service)

---
+++ auth-service/src/main/java/com/springboot/authservice/enums/Role.java
@@ -0,0 +1,5 @@
+package com.springboot.authservice.enums;
+
+public enum Role {
+    ADMIN, USER
+}
---
+++ auth-service/src/main/java/com/springboot/authservice/request/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.springboot.authservice.request;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequest {
+    private String username;
+    private String password;
+}
