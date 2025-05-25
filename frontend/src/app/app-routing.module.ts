3726caaa068caceff66fa535a3816cbecbeb63e0
fcaali <alizoubair3@gmail.com>
2025-05-25 18:01:25 +0100

    Add /profile route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RegistrationSuccessComponent } from './components/register-success/registration-success.component';
-import { ProfilComponent } from './components/user/profil/profil.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import {UserListComponent} from "./components/admin/user-list/user-list.component";
 import {UserAddComponent} from "./components/admin/user-add/user-add.component";
 import {UserEditComponent} from "./components/admin/user-edit/user-edit.component";
@@ -14,11 +14,12 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'register-success', component: RegistrationSuccessComponent },
-  { path: 'profile', component: ProfilComponent },
+  { path: 'profile', component: ProfileComponent },
 
   { path: 'admin/users', component: UserListComponent },
   { path: 'admin/users/add', component: UserAddComponent },
   { path: 'admin/users/edit/:id', component: UserEditComponent },
+  { path: 'admin/profile', component: ProfileComponent },
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' }
