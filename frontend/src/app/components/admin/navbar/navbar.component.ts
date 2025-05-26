d46d49a5867d5bacb2b3d3e3aca8898ab5d7ed50
fcaali <alizoubair3@gmail.com>
2025-05-26 11:31:51 +0100

    Fixed admin navbar component

--- frontend/src/app/components/admin/navbar/navbar.component.ts
+++ frontend/src/app/components/admin/navbar/navbar.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterLinkActive} from "@angular/router";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  standalone: true
+  imports: [
+    RouterLinkActive,
+    [RouterModule]
+  ],
+  standalone: true,
 })
 export class AdminNavbarComponent {}
